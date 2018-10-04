eccb3a92c4f3d02ebebf35cd086a195304fc8edf
alexmorphism <alexandrecastro@alexandres-MacBook-Pro.local>
2018-10-03 22:27:32 -0400

    first commit

---
+++ alex/Places_Everyone.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+/**
+ * Places_Everyone.java
+ * @author alexandrecastro
+ */
+public class Places_Everyone {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        
+        
+        int n1, n2;
+        int count = 0;
+        
+        String c1 = "";
+        String c2 = "";
+        
+        n1 = 77771;
+        n2 = 77177;
+        
+        c1 += n1;
+        c2 += n2;
+        
+        
+        
+    }
+}
+  
+                        
+
+
+
+//System.out.println(n1 + " " + n2);
+//System.out.println("They are positional");
+//System.out.println(n1 + "" +n2);
+//System.out.println("They are not positional");
